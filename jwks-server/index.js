2ceff5253dace0bbf65b35808743b1303bc26a5f
pjs0175 <priyashshah@my.unt.edu>
2024-09-18 21:02:33 -0500

    Initial server setup with Node and Express

---
+++ jwks-server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Basic route to ensure the server is working
+app.get('/', (req,res) => {
+    res.send('JWKS Server is Working');
+});
+
+//start server
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+});
